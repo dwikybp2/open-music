032fb85b64f279915621d2a4aae3a9ef704f5b2f
dwikybp2 <dwikybaguspasetyo@gmail.com>
2023-07-15 14:02:50 +0700

    open music awal

---
+++ src/api/albums/handler.js
@@ -0,0 +1,149 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postAlbumsHandler = this.postAlbumsHandler.bind(this);
+    this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
+    this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
+    this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
+  }
+
+  async postAlbumsHandler(request, h) {
+    try {
+      this._validator.validateAlbumPayload(request.payload);
+      const { name, year } = request.payload;
+
+      const albumId = await this._service.addAlbum({ name, year });
+
+      const response = h.response({
+        status: 'success',
+        data: {
+          albumId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // server ERROR
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kesalahan pada server kami',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async getAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const album = await this._service.getAlbumById(id);
+
+      // const response = h.response({
+      //   status: 'success',
+      //   data: {
+      //     album,
+      //   },
+      // });
+      // response.code(200);
+      // return response;
+      return {
+        status: 'success',
+        data: {
+          album,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kesalahan pada server kami',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async putAlbumByIdHandler(request, h) {
+    try {
+      this._validator.validateAlbumPayload(request.payload);
+      const { id } = request.params;
+      await this._service.editAlbumById(id, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Album berhasil diperbarui',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kesalahan pada server kami',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+
+      await this._service.deleteAlbumById(id);
+
+      return {
+        status: 'success',
+        message: 'Album berhasil dihapus',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kesalahan pada server kami',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+module.exports = AlbumsHandler;
---
+++ src/validator/albums/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { AlbumPayloadSchema } = require('./schema');
+
+const AlbumValidator = {
+  validateAlbumPayload: (payload) => {
+    const validationResult = AlbumPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error);
+    }
+  },
+};
+
+module.exports = AlbumValidator;
